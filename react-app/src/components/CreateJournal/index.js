8b2c780cd4372c7cc680dc451c0d3cc003f2161c
Hieu-Ma <simshieu1@gmail.com>
2021-06-02 19:20:00 -0700

    added journal button

--- react-app/src/components/CreateJournal/index.js
+++ react-app/src/components/CreateJournal/index.js
@@ -4,7 +4,9 @@ import './CreateJournal.css'
 const CreateJournal = () => {
    return (
       <div id="create__journal">
-
+         <div id="journal__border">
+            <div id="create__journal">Create a new journal!</div>
+         </div>
       </div>
    )
 }
