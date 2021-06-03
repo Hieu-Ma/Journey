ea5d6a332649c820d867feecbd24748d23b7bef2
Hieu-Ma <simshieu1@gmail.com>
2021-06-02 20:15:45 -0700

    changed styling of home page and sizing is more dynamic

--- react-app/src/components/CreateJournal/index.js
+++ react-app/src/components/CreateJournal/index.js
@@ -5,7 +5,7 @@ const CreateJournal = () => {
    return (
       <div id="create__journal">
          <div id="journal__border">
-            <div id="create__journal">Create a new journal!</div>
+            <div id="create__button">create a new journal!</div>
          </div>
       </div>
    )
