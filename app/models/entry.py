2957f2c5dd8098830f0ca6a15ada18de814f1a42
Hieu-Ma <simshieu1@gmail.com>
2021-06-03 22:52:09 -0700

    added models and relationships

--- app/models/entry.py
+++ app/models/entry.py
@@ -4,11 +4,11 @@ class Entry(db.Model):
    __tablename__ = 'entries'
 
    id = db.Column(db.Integer, primary_key = True)
-   journal_id = db.Column(db.Integer, db.ForeignKey('journal.id')
-   nullable = False)
-   title = db.Column(db.String(25) nullable = False)
+   journal_id = db.Column(db.Integer, db.ForeignKey('journals.id'), nullable = False)
+   title = db.Column(db.String(25), nullable = False)
    description = db.Column(db.String(1000), nullable = False)
-
+   journals = db.relationship('Journal', back_populates='entries')
+  
    def to_dict(self):
       return {
          "journal_id": self.journal_id,
--- app/models/journal.py
+++ app/models/journal.py
@@ -4,10 +4,10 @@ class Journal(db.Model):
    __tablename__ = 'journals'
 
    id = db.Column(db.Integer, primary_key=True)
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id')
-   nullable=False)
-   title = db.Column(db.String(25) nullable=False)
-   entries = db.relationship('Entry', backref='journal', lazy=True)
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+   title = db.Column(db.String(25), nullable=False)
+   users = db.relationship('User', back_populates='journals')
+   entries = db.relationship('Entry', back_populates='journals', lazy=True)
 
    def to_dict(self):
       return {
