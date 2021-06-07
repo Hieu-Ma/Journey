575846288d920e55644769f3f9071f30abf211f9
Hieu-Ma <simshieu1@gmail.com>
2021-06-07 15:56:29 -0700

    added button to add entry

--- react-app/src/components/ViewJournal/index.js
+++ react-app/src/components/ViewJournal/index.js
@@ -89,8 +89,14 @@ const ViewJournal = () => {
 
    return (
       <div id="journal">
-         <div id="journal__title">
-            {journal.title}
+         <div id="journal__title__container">
+            <button id="create__entry">
+               <svg xmlns="http://www.w3.org/2000/svg" width="23.37" height="23" viewBox="0 0 23.37 23">
+               <line id="Line_6" data-name="Line 6" y2="21" transform="translate(10.846 1)" fill="none" stroke="#646464" stroke-linecap="round" stroke-width="2"/>
+               <line id="Line_7" data-name="Line 7" x2="21.37" transform="translate(1 11.582)" fill="none" stroke="#646464" stroke-linecap="round" stroke-width="2"/>
+               </svg>
+            </button>
+            <div id="journal__title">{journal.title}</div>
          </div>
          <div id="entries__container">
             <div id="journal__entries__container">
